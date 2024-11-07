734c8cb7ad99b80e92ade2cd6b3889ed5917736e
strozw <strozw@users.noreply.github.com>
2024-11-07 09:18:39 +0900

    fix: chnage `setProjectAnnotations` import path to `experimental-nextjs-vite`

--- .storybook/vitest.setup.ts
+++ .storybook/vitest.setup.ts
@@ -1,4 +1,4 @@
-import { setProjectAnnotations } from "@storybook/nextjs";
+import { setProjectAnnotations } from "@storybook/experimental-nextjs-vite";
 import { beforeAll } from "vitest";
 import * as projectAnnotations from "./preview";
 
